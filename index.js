049dd282e76ba44cbb22140610a02fd64084645b
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-05-17 22:50:29 -0300

    name name name

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ client.on('message', message => {
         case 'kick':
             if (!args[1]) message.channel.send('Please Mention the user you are trying to kick')
             if(!message.member.hasPermission('KICK_MEMBERS'))
-            var user = message.mentions.users.first();
+            const  user = message.mentions.users.first();
             if (user) {
                 const member = message.guild.member(user);
 
