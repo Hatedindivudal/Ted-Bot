dfd004f82d210aac5e2de788923ff21317d67ca0
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-05-21 01:59:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -103,11 +103,14 @@ client.on('message', message => {
                     
                     if(message.content === 'sdada'){
                     let newembed = new Discord.MessageEmbed
-                    .setColor('RANDOM')
-                    .setTitle('Wasd')
-                    .addField('Testing', )
+                   
+                    .setColor('0x#ff0000')
+                    .setTitle('Server Information')
+                    .addField('Current Server', message.guild.name)
+                    .addField('Owner', message.guild.owner)
+                    .addField('Created Date', message.guild.createdAt)
+                    .addField('Member count', message.guild.memberCount)
                     message.channel.send(newembed)
-                    
 
                     }
 
