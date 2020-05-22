2fc4ba4c3199e2c37bc3454d28ea7d05409ebcd6
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-05-22 02:40:52 -0300

    xxxx

--- index.js
+++ index.js
@@ -34,15 +34,25 @@ client.on('message', message => {
     if (!message.content.startsWith(PREFIX)) return;
     let args = message.content.substring(PREFIX.length).split(" ");
 
-
     switch (args[0]) {
-        case '.hi':
-            if(!message.member.hasPermission('VIEW_GUILD_INSIGHTS'))
-            message.channel.send('You do not have the odd perrmision command')
-            message.channel.send('..Si')
-
-
+        case 'meme':
+    const { body } = await snekfetch
+            .get('https://www.reddit.com/r/dankmemes.json?sort=top&t=week')
+            .query({ limit: 800 });
+        const allowed = message.channel.nsfw ? body.data.children : body.data.children.filter(post => !post.data.over_18);
+        if (!allowed.length) return message.channel.send('It seems we are out of fresh memes!, Try again later.');
+        const randomnumber = Math.floor(Math.random() * allowed.length)
+        const Meme = new Discord.MessageEmbed()
+        .setColor(0x00A2E8)
+        .setTitle(allowed[randomnumber].data.title)
+        .setDescription("Posted by: " + allowed[randomnumber].data.author)
+        .setImage(allowed[randomnumber].data.url)
+        .addField("Other info:", "Up votes: " + allowed[randomnumber].data.ups + " / Comments: " + allowed[randomnumber].data.num_comments)
+        .setFooter("Memes provided by r/dankmemes")
+        message.channel.send(Meme)
+   
     }
+    
 
 
 
