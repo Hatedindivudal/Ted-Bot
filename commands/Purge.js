880ae006924a77cf001e881c13bde394c146a6a9
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-05-24 19:19:50 -0300

    xd

--- commands/Purge.js
+++ commands/Purge.js
@@ -1,27 +1,19 @@
-const Discord = require('discord.js');
-    module.exports = {
-
-      
-
-        
-        execute(message, args) {
-            switch (args[0]) {
-        case 'Purge':
-            
-        
-            if (!args[1]) return message.reply('Error Please define second arg')
-            if(!message.member.hasPermission('MANAGE_MESSAGES'))
-            message.channel.send('You do not have the perms to do so')
-            message.channel.bulkDelete(args[1]);
-
-            
-            
-            
-        
-
-
-        }
-    },
-    
-
-    };
+const Discord = require("discord.js");
+const amount = parseInt(args[0]);
+module.exports = {
+  name: "purge",
+  description: "deletes messages!",
+  execute(message, args) {
+    if (isNaN(amount)) {
+      return message.reply("that doesn't seem to be a valid number.");
+    }
+    if (isNaN(amount)) {
+      return message.reply("that doesn't seem to be a valid number.");
+    } else if (amount < 2 || amount > 100) {
+      return message.reply("you need to input a number between 2 and 100.");
+    }
+    if (!message.member.hasPermission("MANAGE_MESSAGES"))
+    return message.channel.send("You do not have the perms to do so");
+    message.channel.bulkDelete(amount);
+  }
+};
