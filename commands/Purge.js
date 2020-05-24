963c72ca0885032eb9c164878dac6985e28bc162
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-05-24 16:35:14 -0300

    nasbdiaskdm

--- commands/Purge.js
+++ commands/Purge.js
@@ -1,21 +1,27 @@
-const Discord = require('discord.js');
- module.exports = {
-name: 'purge',
-description: 'Deletes messages'
-execute(message, args) {
-    const args = message.content.split(' ').slice(1); 
-    const amount = args.join(' '); 
-    
-    if (!amount) return msg.reply('You haven\'t given an amount of messages which should be deleted!'); 
-    if (isNaN(amount)) return msg.reply('The amount parameter isn`t a number!');
-    
-    if (amount > 100) return msg.reply('You can`t delete more than 100 messages at once!'); 
-    if (amount < 1) return msg.reply('You have to delete at least 1 message!');
-    
-    await msg.channel.messages.fetch({ limit: amount }).then(messages => { 
-        msg.channel.bulkDelete(messages 
-    )});
+const Discord = require('discord.js');k
+    module.exports = {
+
+      
+
+        
+        execute(message, args) {
+            switch (args[0]) {
+        case 'Purge':
+            
+        
+            if (!args[1]) return message.reply('Error Please define second arg')
+            if(!message.member.hasPermission('MANAGE_MESSAGES'))
+            message.channel.send('You do not have the perms to do so')
+            message.channel.bulkDelete(args[1]);
+
+            
+            
+            
+        
+
 
         }
+    },
+    
 
-    }
+    };
