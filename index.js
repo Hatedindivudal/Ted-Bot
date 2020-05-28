88651ee200aba7df88888a28b30a8fe035dba97e
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-05-27 23:54:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,200 +1,45 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Command } = require("discord.js-commando");
 
-
-
-const PREFIX = '.';
-
-const embed = new Discord.MessageEmbed();
-
-const fs = require('fs');
-client.commands = new Discord.Collection();
- 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
-}
-
-const ms = require('ms');
-
-client.on('ready', () => {
-    console.log('attepmt #2');
-
-})
-
-client.on('message', message => {
-
-
-    const usersMap = new Map();
-
-    
-
-    if (!message.content.startsWith(PREFIX)) return;
-    let args = message.content.substring(PREFIX.length).split(" ");
-
-
-
-
-
-    switch (args[0]) {
-        case '.hi':
-            if(!message.member.hasPermission('VIEW_GUILD_INSIGHTS'))
-            message.channel.send('You do not have the odd perrmisions')
-            message.channel.send('..Si')
-
-
-    }
-
-switch (args[0]) {
-    case '.@everyone':
-        message.channel.send('..veryone')
-    
-    
-    
-}
-
-    switch (args[0]) {
-        case 'kick':
-            if(!message.member.hasPermission('KICK_MEMBERS'))
-            message.channel.send('You do not have the Kick user perms')
-            const  user = message.mentions.users.first();
-            if (user) {
-                const member = message.guild.member(user);
-
-                if (member) {
-
-                    member.kick('You Have Been Removed from the server').then(() => {
-                        message.reply(`Sucessfully Kicked ${user.tag}`);
-                    }).catch(Err => {
-                        message.reply('i was unable to complete that command.')
-                        console.log(err);
-                    });
-                } else {
-                    message.reply('I Could not find that user inside this server.')
-
-
-                }
-
-
-            } else {
-                message.reply('I Could not find that user inside this server.')
-            }
-        } 
-        switch (args[0]) {
-         case 'purge':
-            client.commands.get('Purge').execute(message, args)
-            break;
-
-
+module.exports = class PurgeCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: "purge",
+      group: "moderation",
+      memberName: "purge",
+      description: "Purges the Chat",
+      clientPermissions: ["MANAGE_MESSAGES"],
+      userPermissions: ["MANAGE_MESSAGES"],
+      args: [
+        {
+          key: "purgecount",
+          prompt: "How many messages should I purge??",
+          type: "integer"
         }
-    
-      
+      ],
+      guildOnly: true
+    });
+  }
 
-        
-                switch (args[0]) {
-                    case 'ban':
-                     
-                       if(!message.member.hasPermission('BAN_MEMBERS'))
-                       message.channel.send("You do not have Ban perms")
-                       const user = message.mentions.users.first();
+  async run(message, args) {
+    // Purge Command!
+    if (message.author.bot) return;
+    if (args.purgecount > 100)
+      return message.reply(
+        "You can currently only purge up to 100 messages at a time."
+      );
 
-                        if (user) {
-                            const member = message.guild.member(user);
-
-                            if (member) {
-                                member.ban({
-                                    ression: 'You have been removed from the server'
-                                }).then(() => {
-                                    message.reply(`${user.tag} Has been removed from the server!`)
-
-
-                                })
-                            } else {
-                                message.reply('I Could not find that user inside this server.')
-
-
-                            }
-
-
-                        } else {
-                            message.reply('I Could not find that user inside this server.')
-                            break;
-                        }
-                    }
-
-                    
-                    if(message.content === 'test'){
-                    let newembed = new Discord.MessageEmbed()
-                   
-                    .setColor('0x#ff0000')
-                    .setTitle('Server Information')
-                    .addField('Current Server', message.guild.name)
-                    .addField('Owner', message.guild.owner)
-                    .addField('Created Date', message.guild.createdAt)
-                    .addField('Member count', message.guild.memberCount)
-                    message.channel.send(newembed)
-
-                    }
-
-
-                            switch (args[0]) {
-                                case 'ping':
-                                    client.commands.get('ping').execute(message, args)
-                                    break;
-                                 
-                            }
-
-
-                                    switch (args[0]) {
-                                        case 'Mute':
-                                           client.commands.get('Mute').execute(message, args);
-                                           break
-                            
-                                case 'website':
-                                    message.channel.send('Sorry i do not currently own one please check back later!');
-                                    break;
-                                case 'info':
-                                    if (args[1] === 'version') {
-                                        message.channel.send('We are Currently using version 1.0.1');
-                                    } else {
-                                        message.channel.send('Invalid args')
-                                    }
-                                    break;
-                                {
-
-                                    
-                                }
-                                case 'Serverinfo':
-                                    let embed = new Discord.MessageEmbed()
-
-                                        .setColor('0x#ff0000')
-                                        .setTitle('Server Information')
-                                        .addField('Current Server', message.guild.name)
-                                        .addField('Owner', message.guild.owner)
-                                        .addField('Created Date', message.guild.createdAt)
-                                        .addField('Member count', message.guild.memberCount)
-                                        .setThumbnail(message.guild.iconURL)
-                                        
-                                    message.channel.send(embed);
-                                    break;
-                                    
-                                    
-
-                        
-                            }
-
-                        
-
-
-                
-
-
-            
-                                    
-
-});
-
-
-
-client.login(process.env.token);
+    await message.channel.messages
+      .fetch({ limit: args.purgecount })
+      .then(async messages => {
+        // Fetches the messages
+        await message.channel.bulkDelete(messages);
+      })
+      .then(() => {
+        message
+          .reply(`🗑️ Sucessfully Deleted ${args.purgecount} messages.`)
+          .then(async e => {
+            await e.delete(2000);
+          });
+      });
+  }
+};
