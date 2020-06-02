09b728f8ea343a5540a972691a180dae0e4ea27f
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-06-01 23:34:26 -0300

    Update Purge.js

--- commands/Purge.js
+++ commands/Purge.js
@@ -1,39 +1,38 @@
-const Discord = require('discord.js');
-
-
-
 module.exports = {
-    name: 'purge',
-    description: "Deletes messages",
+    name: "clear",
+    aliases: ["purge", "nuke"],
+    category: "moderation",
+    description: "Clears the chat",
+    execute(message, args) {
+        if (message.deletable) {
+            message.delete();
+        }
     
-    
+        // Member doesn't have permissions
+        if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+            return message.reply("You can't delete messages....").then(m => m.delete(5000));
+        }
 
-  execute(message, args) {
+        // Check if args[0] is a number
+        if (isNaN(args[0]) || parseInt(args[0]) <= 0) {
+            return message.reply("Yeah.... That's not a numer? I also can't delete 0 messages by the way.").then(m => m.delete(5000));
+        }
 
+        // Maybe the bot can't delete messages
+        if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) {
+            return message.reply("Sorryy... I can't delete messages.").then(m => m.delete(5000));
+        }
 
+        let deleteAmount;
 
-  if (message.deletable) {
-      message.delete();
-  }
+        if (parseInt(args[0]) > 100) {
+            deleteAmount = 100;
+        } else {
+            deleteAmount = parseInt(args[0]);
+        }
 
-  if (!message.member.hasPermission("MANAGE_MESSAGES")) {
-      return message.reply("Missing Permissions!").then(m => m.delete(5000));
-  }
-
-  if (isNaN(args[0]) || parseInt(args[0]) <= 0) {
-      return message.reply("This is not a number").then(m => m.delete(5000));
-  }
-
-  let deleteAmount;
-  if (parseInt(args[0]) > 100) {
-      deleteAmount = 100;
-  } else {
-      deleteAmount = parseInt(args[0]);
-  }
-
-  message.channel.bulkDelete(deleteAmount, true)
-  .catch(err => message.reply(`Something went wrong... ${err}`));
-
-  }
-
-  }
+        message.channel.bulkDelete(deleteAmount, true)
+            .then(deleted => message.channel.send(`I deleted \`${deleted.size}\` messages.`))
+            .catch(err => message.reply(`Something went wrong... ${err}`));
+    }
+}
