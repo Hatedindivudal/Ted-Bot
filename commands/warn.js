c47f1ed6aa2c7ebed46d07d5d556f595f426aabd
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-06-02 23:42:53 -0300

    '

---
+++ commands/warn.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const fs = require("fs");
+module.exports = {
+    name: 'warn',
+    description: "Warns a user",
+    execute(message, args) {
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You don't have premission to do that!");
+  let reason = args.slice(1).join(' ');
+  let user = message.mentions.users.first();
+  if (message.mentions.users.size < 1) return message.reply('You must mention someone to warn them.');
+  if (reason.length < 1) return message.reply('You must have a reason for the warning.');
+
+  let dmsEmbed = new Discord.RichEmbed()
+  .setTitle("Warn")
+  .setColor("#00ff00")
+  .setDescription(`You have been warned on \`${message.guild.name}\``)
+  .addField("Warned by", message.author.tag)
+  .addField("Reason", reason);
+
+  user.send(dmsEmbed);
+
+  message.delete();
+  
+  message.channel.send(`${user.tag} has been warned`)
+
+}
+
+
+ 
+};
