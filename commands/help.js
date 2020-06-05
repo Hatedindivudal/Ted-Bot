8065cf7f139807f8650ab15eb849c73540cd57db
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-06-05 01:36:50 -0300

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,73 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: 'purge',
+    description: "Deletes a specified amount of numbers",
+    execute(message, args) {
+
+
+
+        const Helpembed = new Discord.MessageEmbed()
+        .setColor('RANDOM')
+        .setAuthor(`${message.guild.me.displayName} Help`, message.guild.iconURL)
+        .setThumbnail(client.user.displayAvatarURL)
+
+
+
+        if(!args[0]) {
+const categories = readdirSync("./commands/")
+
+Helpembed.setDescription(`These are the avaible commands for ${message.guild.me.displayName}/n the bot command prefix is **.**`)
+.setFooter(` ${message.guild.me.displayName} | Total Commands: ${client.commands.size}`, client.user.displayAvatarURL);
+message.channel.send(Helpembed)
+
+
+categories.forEach()
+
+
+            
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
