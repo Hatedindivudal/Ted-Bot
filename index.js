338cfa5a70483ca3d20f49dfdb81560494dc4092
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-06-23 08:55:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ client.on('message', message => {
 
                                     
                                     case 'bye':
-                                        message.channel.setNSFW
+                                        message.channel.setName('Noodles')
                                         break;
                                     
 
