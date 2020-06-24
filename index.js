1bd0ba9967be3c9e7f489d8898cb0842f21812d3
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-06-24 03:21:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ client.on('message', message => {
 
 
     if(!message.content.startsWith(prefix)) return;
-    let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)))
-    if(commandfile) commandfile.run(bot,message,args)
+    let commandfile = client.commands.get(cmd.slice(prefix.length)) || client.commands.get(client.aliases.get(cmd.slice(prefix.length)))
+    if(commandfile) commandfile.run(client,message,args)
 })
 client.login(process.env.token);
