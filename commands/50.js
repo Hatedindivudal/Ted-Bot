5228261709ad38aa2b87c6501d0930ddf1065530
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-06-25 02:49:50 -0300

    Update 50.js

--- commands/50.js
+++ commands/50.js
@@ -4,7 +4,7 @@ const randomPuppy = require('random-puppy');
 module.exports.run = async (bot, message, args) => {
     if(!message.channel.NSFW)
     message.channel.send("This channel is not NSFW")
-    const subReddits = ["FiftyFifty"]
+    const subReddits = ["FiftyFifty", "5050pics", "cursedimages" ]
     const random = subReddits[Math.floor(Math.random() * subReddits.length)];
     const img = await randomPuppy(random);
 
