1f0592745ce55fd6c9489d8a5b4754b984aee333
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-06-25 03:19:20 -0300

    Create newfile.js

---
+++ commands/newfile.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+module.exports.run = async (bot, message, args) => {
+
+
+message.channel.name('noodleman')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports.config = {
+    name: "name",
+    description: "",
+    usage: ".name",
+    accessableby: "Members",
+    aliases: []
+}
+}
