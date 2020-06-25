972e6436c8a716740abd0ecde87da7ee122afeab
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-06-25 00:42:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -23,13 +23,25 @@ const IGNORED = new Set([
 
   const fs = require('fs');
   bot.commands = new Discord.Collection();
-   
-  const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-  for (const file of commandFiles) {
-      const command = require(`./commands/${file}`);
-      bot.commands.set(command.name, command);
-  }
- 
+bot.aliases = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+
+    if(err) console.log(err)
+
+    let jsfile = files.filter(f => f.split(".").pop() === "js") 
+    if(jsfile.length <= 0) {
+         return console.log("[LOGS] Couldn't Find Commands!");
+    }
+
+    jsfile.forEach((f, i) => {
+        let pull = require(`./commands/${f}`);
+        bot.commands.set(pull.config.name, pull);  
+        pull.config.aliases.forEach(alias => {
+            bot.aliases.set(alias, pull.config.name)
+        });
+    });
+});
 
 const ms = require('ms');
 
