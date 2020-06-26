1c143facaa42c6941d88c4b20ddc0485652e8df8
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-06-25 23:55:08 -0300

    Update 50.js

--- commands/50.js
+++ commands/50.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const randomPuppy = require('random-puppy');
 
 module.exports.run = async (bot, message, args) => {
-    if(!message.channel.nsfw) return;
 
     const subReddits = [ "cursedimages" ]
     const random = subReddits[Math.floor(Math.random() * subReddits.length)];
