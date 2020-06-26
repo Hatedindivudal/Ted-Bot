e6a751a6480d69ba8a9a2c852b6c0cbd39df33cc
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-06-26 18:21:28 -0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,37 +1,36 @@
-const Discord = require("discord.js");
-
-module.exports.run = async (bot, message, args) => {  
-
-    //We have to set a argument for the help command beacuse its going to have a seperate argument.
-    let helpArray = message.content.split(" ");
-    let helpArgs = helpArray.slice(1);
 
 
-    //Reads the moudle.exports.config (This line of code is on commands folder, each command will read automaticly) by the second argument (the command name) and shows the information of it.
-    if(helpArgs[0]) {
-        let command = helpArgs[0];
-
-        if(bot.commands.has(command)) {
-            
-            command = bot.commands.get(command);
-            var embed = new Discord.MessageEmbed()
-            .setAuthor(`${command.config.name} Command`)
-            .setDescription(`
-            - **Command's Description** __${command.config.description || "There is no Description for this command."}__
-            - **Command's Usage:** __${command.config.usage || "No Usage"}__
-            - **Command's Permissions:** __${command.config.accessableby || "Members"}__
-            - **Command's Aliases:** __${command.config.aliases || "No Aliases"}__
-            `)
-            .setColor('#2EFF00')
-
-        message.channel.send(embed);
-    }}
-}
-
-module.exports.config = {
-    name: "help",
-    description: "",
-    usage: ".help",
-    accessableby: "Members",
-    aliases: []
-}
+const Discord = require('discord.js');
+module.exports.run = async (bot, message, args) => {    const embed = new Discord.MessageEmbed()
+      .setTitle("Commands:")
+      .addField(
+        "=kick",
+        "Kicks a mentioned user"
+      )
+      .addField(
+        "=ban",
+        "Description: This "
+      )
+      .addField(
+        "=avatar",
+        "Description: Sends a picture of your avatar. (Work in progress, not done yet.)"
+      ) 
+      .addField(
+        "=ping",
+        "Description: Pretty self-explanatory? Ain't it?"
+      )
+      .addField(
+        "=purge",
+        "Description: This command deletes messages (Work in progress, not done yet.)"
+      )
+      .addField(
+        "=say",
+        "Description: This command is for fun use. Do something like =say You suck and the bot will be the one replying."
+      )
+      .addField(
+        "=whois",
+        "Description: This command will give you information on a user."
+      );
+    message.author.send(embed);
+    message.channel.send('Help commands sent in dms!');
+    };
