23c1f840bbc0079df7d66da6159885e9d2903972
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-06-26 21:34:38 -0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,35 +2,9 @@
 
 const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {    const embed = new Discord.MessageEmbed()
-      .setTitle("Commands:")
-      .addField(
-        "=kick",
-        "Kicks a mentioned user"
-      )
-      .addField(
-        "=ban",
-        "Description: This "
-      )
-      .addField(
-        "=avatar",
-        "Description: Sends a picture of your avatar. (Work in progress, not done yet.)"
-      ) 
-      .addField(
-        "=ping",
-        "Description: Pretty self-explanatory? Ain't it?"
-      )
-      .addField(
-        "=purge",
-        "Description: This command deletes messages (Work in progress, not done yet.)"
-      )
-      .addField(
-        "=say",
-        "Description: This command is for fun use. Do something like =say You suck and the bot will be the one replying."
-      )
-      .addField(
-        "=whois",
-        "Description: This command will give you information on a user."
-      );
+    .setTitle('Commands')
+    .setColor("RANDOM")
+    .addField('test123', 'fucking simp im testing i said dumbass mf')
     message.author.send(embed);
     message.channel.send('Help commands sent in dms!');
     };
