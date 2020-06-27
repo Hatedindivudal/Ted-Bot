9ec3028f7945823850f8525c2b1f9e2b4aee710a
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-06-26 21:36:06 -0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,10 +1,21 @@
 
 
 const Discord = require('discord.js');
-module.exports.run = async (bot, message, args) => {    const embed = new Discord.MessageEmbed()
+module.exports.run = async (bot, message, args) => {    
+    const embed = new Discord.MessageEmbed()
     .setTitle('Commands')
     .setColor("RANDOM")
     .addField('test123', 'fucking simp im testing i said dumbass mf')
     message.author.send(embed);
     message.channel.send('Help commands sent in dms!');
     };
+
+    module.exports.config = {
+        name: "help",
+        description: "",
+        usage: "=help",
+        accessableby: "Members",
+        aliases: []
+    }
+
+
