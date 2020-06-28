579be1dba187b8f8ce23ad1c12568f27ad5ddc7d
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-06-28 00:35:58 -0300

    Update uban.js

--- commands/uban.js
+++ commands/uban.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js');
     module.exports.run = async (bot, message, args) => {
         if(!message.member.hasPermission(["BAN_MEMBERS"])) return message.channel.send("You dont have permission to perform this command!")
         if(isNaN(args[0])) return message.channel.send("You need to provide an ID.")
-        let bannedMember =  client.users.fetch(args[0])
+        let bannedMember =  bot.users.fetch(args[0])
             if(!bannedMember) return message.channel.send("Please provide a user id to unban someone!")
     
         let reason = args.slice(1).join(" ")
