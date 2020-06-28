44960668a835996ddbbb205cddcdb5f03c6806d9
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-06-27 23:11:23 -0300

    Update uban.js

--- commands/uban.js
+++ commands/uban.js
@@ -2,80 +2,46 @@
 const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
     
-    let unbanned = message.mentions.users.first() || bot.users.resolve(args[0]);
-    let reason = args.slice(1).join(" ");
+    module.exports.run = async (bot, message, args) => {
 
-    let member = await bot.users.fetch(unbanned);
-    let ban = await message.guild.fetchBans();
-
-    // MESSAGES
-
-    if (!unbanned) {
-      let unbaninfoembed = new Discord.MessageEmbed()
-        .setTitle("Command: unban")
-        .setDescription(
-          `**Description:** Unban a member. \n` +
-            "**Sub Commands:**\n" +
-            "" +
-            "**Usage:**\n" +
-            "-unban [user] (limit) (reason) \n" +
-            "**Examples:** \n" +
-            "-unban <@597253939469221891> good guy \n" +
-            "-unban 597253939469221891 good guy "
-        )
-        .setColor("#2C2F33");
-      message.channel.send(unbaninfoembed);
-
-      return;
+        if(!message.member.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send("You dont have permission to perform this command!")
+    
+        let bannedMember = await bot.fetchUser(args[0])
+            if(!bannedMember) return message.channel.send("Please provide a user id to unban someone!")
+    
+        let reason = args.slice(1).join(" ")
+            if(!reason) reason = "No reason given!"
+    
+        if(!message.guild.me.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send("I dont have permission to perform this command!")|
+        message.delete()
+        try {
+            message.guild.unban(bannedMember, {reason: reason})
+            message.channel.send(`${bannedMember.tag} has been unbanned from the guild!`)
+        } catch(e) {
+            console.log(e.message)
+        }
+    
+        let embed = new Discord.RichEmbed()
+        .setColor(colours.redlight)
+        .setAuthor(`${message.guild.name} Modlogs`, message.guild.iconURL)
+        .addField("Moderation:", "unban")
+        .addField("Moderated on:", `${bannedMember.username} (${bannedMember.id})`)
+        .addField("Moderator:", message.author.username)
+        .addField("Reason:", reason)
+        .addField("Date:", message.createdAt.toLocaleString())
+        
+            let sChannel = message.guild.channels.find(c => c.name === "tut-modlogs")
+            sChannel.send(embed)
+    
     }
-
-    if (!ban.get(member.id)) {
-      let notbannedembed = new Discord.MessageEmbed()
-        .setDescription("This user is not banned")
-        .setColor("#2C2F33");
-      message.channel.send(notbannedembed);
-
-      return;
+    
+    module.exports.config = {
+        name: "unban",
+        description: "Unban a user from the guild!",
+        usage: "!unban",
+        accessableby: "Administrators",
+        aliases: ["ub", "unbanish"]
     }
-
-    if (!message.guild.me.permissions.has("BAN_MEMBERS")) {
-      let botnoperms = new Discord.MessageEmbed()
-        .setDescription(
-          "I do not have permissions, please contact an administrator"
-        )
-        .setColor("#2C2F33");
-      message.channel.send(botnoperms);
-
-      return;
-    }
-
-    if (!message.member.permissions.has("BAN_MEMBERS")) {
-      let nopermsembed = new Discord.MessageEmbed()
-        .setDescription(
-          "You do not have permission `BAN MEMBERS` contact an administrator"
-        )
-        .setColor("#2C2F33");
-      message.channel.send(nopermsembed);
-
-      return;
-    }
-
-    var user = ban.get(member.id);
-    message.guild.members.unban(member);
-    let successfullyembed = new Discord.MessageEmbed()
-      .setTitle(`${member.tag} has been successfully unbanned.`)
-      .setColor("#2C2F33");
-
-    message.channel.send(successfullyembed);
 
 
 }
-
-module.exports.config = {
-    name: "unban",
-    description: "",
-    usage: ".unban",
-    accessableby: "Members",
-    aliases: []
-}
-
