c85bfeb7a6eddbf536a314bfdfeb25f6973fef88
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-06-28 00:32:57 -0300

    Update uban.js

--- commands/uban.js
+++ commands/uban.js
@@ -2,37 +2,25 @@
 const Discord = require('discord.js');
     
     module.exports.run = async (bot, message, args) => {
-
-        if(!message.member.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send("You dont have permission to perform this command!")
-    
-        let bannedMember = await bot.users.fetch(args[0])
+        if(!message.member.hasPermission(["BAN_MEMBERS"])) return message.channel.send("You dont have permission to perform this command!")
+        if(isNaN(args[0])) return message.channel.send("You need to provide an ID.")
+        let bannedMember =  client.users.fetch(args[0])
             if(!bannedMember) return message.channel.send("Please provide a user id to unban someone!")
     
         let reason = args.slice(1).join(" ")
             if(!reason) reason = "No reason given!"
     
-        if(!message.guild.me.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send("I dont have permission to perform this command!")|
+        if(!message.guild.me.hasPermission(["BAN_MEMBERS"])) return message.channel.send("I dont have permission to perform this command!")|
         message.delete()
         try {
-            message.guild.unban(bannedMember, {reason: reason})
-            message.channel.send(`${bannedMember.tag} has been unbanned from the guild!`)
+            message.guild.members.unban(bannedMember, reason)
+            message.channel.send(`**${bannedMember.tag}** has been unbanned from the guild!`)
         } catch(e) {
             console.log(e.message)
         }
-    
-        let embed = new Discord.MessageEmbed()
-        .setColor(colours.redlight)
-        .setAuthor(`${message.guild.name} Modlogs`, message.guild.iconURL)
-        .addField("Moderation:", "unban")
-        .addField("Moderated on:", `${bannedMember.username} (${bannedMember.id})`)
-        .addField("Moderator:", message.author.username)
-        .addField("Reason:", reason)
-        .addField("Date:", message.createdAt.toLocaleString())
-        
-            let sChannel = message.guild.channels.find(c => c.name === "tut-modlogs")
-            sChannel.send(embed)
-    
     }
+    
+
     
     
     
