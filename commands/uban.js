8731774c7794cd95008ac0c21065f7d59aabc9d1
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-06-27 21:44:21 -0300

    Create uban.js

---
+++ commands/uban.js
@@ -0,0 +1,47 @@
+
+const Discord = require('discord.js');
+module.exports.run = async (bot, message, args) => {
+    
+
+    if(!message.member.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send("You can't do that.")
+
+    if(!args[0]) return message.channel.send("Give me a valid ID"); 
+    //This if() checks if we typed anything after "!unban"
+
+    let bannedMember;
+    //This try...catch solves the problem with the await
+    try{                                                            
+        bannedMember = await bot.users.fetch(args[0])
+    }catch(e){
+        if(!bannedMember) return message.channel.send("That's not a valid ID")
+    }
+
+    //Check if the user is not banned
+    try {
+            await message.guild.fetchBan(args[0])
+        } catch(e){
+            message.channel.send('This user is not banned.');
+            return;
+        }
+
+    let reason = args.slice(1).join(" ")
+    if(!reason) reason = "..."
+
+    if(!message.guild.me.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send("I can't do that")
+    message.delete()
+    try {
+        message.guild.members.unban(bannedMember, {reason: reason})
+        message.channel.send(`${bannedMember.tag} was readmitted.`)
+    } catch(e) {
+        console.log(e.message)
+    }
+}
+
+module.exports.config = {
+    name: "unban",
+    description: "",
+    usage: ".unban",
+    accessableby: "Members",
+    aliases: []
+}
+
