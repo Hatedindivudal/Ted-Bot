3b80e2e9c09a7fa7db052090e6485cb210b032f6
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-06-28 00:41:11 -0300

    Update uban.js

--- commands/uban.js
+++ commands/uban.js
@@ -7,13 +7,11 @@ const Discord = require('discord.js');
         let bannedMember =  bot.users.fetch(args[0])
             if(!bannedMember) return message.channel.send("Please provide a user id to unban someone!")
     
-        let reason = args.slice(1).join(" ")
-            if(!reason) reason = "No reason given!"
     
         if(!message.guild.me.hasPermission(["BAN_MEMBERS"])) return message.channel.send("I dont have permission to perform this command!")|
         message.delete()
         try {
-            message.guild.members.unban(bannedMember, reason)
+            message.guild.members.unban(bannedMember)
             message.channel.send(`**${bannedMember.tag}** has been unbanned from the guild!`)
         } catch(e) {
             console.log(e.message)
