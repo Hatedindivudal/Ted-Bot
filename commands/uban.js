afe9db88c7a659399dbd12e3777a5a699873600b
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-06-28 22:23:22 -0300

    Update uban.js

--- commands/uban.js
+++ commands/uban.js
@@ -26,8 +26,7 @@ module.exports.run = async (bot, message, args) => {
 
     
 
-    if(!message.guild.me.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send("I can't do that")
-    message.delete()
+    
     try {
         message.guild.members.unban(bannedMember)
         message.channel.send(`${bannedMember.tag} was readmitted.`)
