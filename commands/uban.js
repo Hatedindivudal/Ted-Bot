84c7de57d72f51fb58074236dcd5433998936434
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-06-29 01:45:57 -0300

    Update uban.js

--- commands/uban.js
+++ commands/uban.js
@@ -5,7 +5,7 @@ module.exports.run = (bot, message, args) => {
     if(!message.member.hasPermission('BAN_MEMBERS'))
     message.channel.send("You do not have Ban perms")
     message.guild.members.unban('309996535758979072')
-    .then(user => console.log(`Unbanned ${user.username} from ${guild.name}`))
+
     .catch(console.error);
 }
     
