e58213797f64c6b7075ac9b659da97b1ffbd0c57
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-06-28 22:39:59 -0300

    Update uban.js

--- commands/uban.js
+++ commands/uban.js
@@ -1,36 +1,19 @@
 
 const Discord = require('discord.js');
     
-module.exports.run = async (bot, message, args) => { 
+module.exports.run = (bot, message, [id]) => {
+    if (!message.member.roles.cache.find(r => r.name === "UNBAN_PERMS")) return message.channel.send('❌**You do not have perms**')
 
-    if(!message.member.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send("You can't do that.")
+    if (id) {
+        message.guild.members.unban(id).then(() => {
+            message.reply('✅**Succesful**');
+        }).catch((e) => {
+            message.reply('❌ **Something went wrong**')
+            console.error(e)
+        })
 
-    if(!args[0]) return message.channel.send("Give me a valid ID"); 
-    //This if() checks if we typed anything after "!unban"
-
-    let bannedMember;
-    try{                                                            
-        bannedMember = await bot.users.fetch(args[0])
-    }catch(e){
-        if(!bannedMember) return message.channel.send("That's not a valid ID")
-    }
-
-    //Check if the user is not banned
-    try {
-            await message.guild.fetchBan(args[0])
-        } catch(e){
-            message.channel.send('This user is not banned.');
-            return;
-        }
-
-    
-
-    
-    try {
-        message.guild.members.unban(bannedMember)
-        message.channel.send(`${bannedMember.tag} was readmitted.`)
-    } catch(e) {
-        console.log(e.message)
+    } else {
+        message.reply('❌**You need to specify a person**');
     }
 }
     
