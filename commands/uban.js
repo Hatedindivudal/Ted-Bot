57b170981690aed31862f5292bd2c286026af4a4
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-06-29 01:42:38 -0300

    Update uban.js

--- commands/uban.js
+++ commands/uban.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js');
 module.exports.run = (bot, message, args) => {
     if(!message.member.hasPermission('BAN_MEMBERS'))
     message.channel.send("You do not have Ban perms")
-    guild.members.unban('309996535758979072')
+    message.guild.members.unban('309996535758979072')
     .then(user => console.log(`Unbanned ${user.username} from ${guild.name}`))
     .catch(console.error);
 }
