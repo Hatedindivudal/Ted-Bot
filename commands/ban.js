396c821de8571d3a8e91274a2ee8f377a98056dc
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-07-05 01:33:07 -0300

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,20 +1,41 @@
 const Discord = require("discord.js")
 module.exports.run = async (bot, message, args) => {
-    if(!message.member.hasPermission('BAN_MEMBERS')) 
-        message.channel.send("You do not have Permission");
-    else {
-        let bannedMember = await message.guild.members.ban(args);
-        if(bannedMember){
+    let xdemb = new Discord.MessageEmbed()
+    .setColor("#00ff00")
+    .setTitle("Ban Command")
+    .addField("Description:", `Ban a member`, true)
+    .addField("Usage:", `!ban [user] [reason]`, true)
+    .addField("Example:", `!ban @Randomexample#7183`)
 
-        try {
-            console.log(bannedMember.tag + " has been removed");
-            message.channel.send (`${bannedMember} has been kicked!`)
-        }
-            catch(err) {
-            console.log(err);
-        }
+    if(!message.member.hasPermission('KICK_MEMBERS'))
+    message.channel.send("You don't have permission to use that command.");
+    let member = message.mentions.members.first();
+    if(!member) return message.channel.send(xdemb)
+    if(!member.bannable) return message.channel.send("I can't ban this user!")
+
+    if(member.id === message.author.id) return message.channel.send("You can't ban your self")
+
+    let reason = args.slice(1).join(" ");
+
+    if(!reason) {
+        res = "No reason given";
+    } else {
+        res = reason
     }
-}
+
+    await member.ban(reason).catch(error => message.channel.send(`Sorry, I coldn't ban because of: ${error}`));
+
+    let bean = new Discord.MessageEmbed()
+    .setColor("#00ff00")
+    .setTitle(`Ban | ${member.user.tag}`)
+    .addField("User", member, true)
+    .addField("Moderator", message.author, true)
+    .addField("Reason", res)
+    .setTimestamp()
+
+    message.channel.send(bean)
+
+    message.delete()
 }
 
 module.exports.config = {
