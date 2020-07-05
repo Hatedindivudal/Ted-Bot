41119f919ddcaea9d17c31e029f6f62a8d26929f
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-07-05 01:59:12 -0300

    Create test.123.js

---
+++ commands/test.123.js
@@ -0,0 +1,53 @@
+const Discord = require('discord.js');
+module.exports.run = async (bot, message, args) => {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    channel.updateOverwrite(channel.guild.roles.everyone, { SEND_MESSAGES: false });
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+module.exports.config = {
+    name: "test",
+    description: "",
+    usage: ".test",
+    accessableby: "Members",
+    aliases: []
+}
