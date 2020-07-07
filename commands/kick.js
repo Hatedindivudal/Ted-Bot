0defe4cdfc1928ed3c8c1c3642c9172c3bee4183
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-07-06 22:26:50 -0300

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,7 +1,7 @@
 
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-    if (!message.member.hasPermission("KICK_MEMBERS")) 
+    if (!message.member.hasPermission("KICK_MEMBERS"));
     const xdemb = new Discord.MessageEmbed()
     .setColor("#00ff00")
     .setTitle("Kick Command")
