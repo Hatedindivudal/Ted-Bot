f4f8f0f0eaff35d47098fa0b30b438f7a7249107
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-07-06 23:50:31 -0300

    n

--- commands/kick.js
+++ commands/kick.js
@@ -1,34 +1,32 @@
-
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     if (!message.member.hasPermission("KICK_MEMBERS"));
     const xdemb = new Discord.MessageEmbed()
-    .setColor("#00ff00")
-    .setTitle("Kick Command")
-    .addField("Description:", `Kick a member`, true)
-    .addField("Usage:", "!kick [user] [reason]", true)
-    .addField("Example:" ,"!kick @hackernub123 spam")
-  
-      const member = message.mentions.members.first();
-      if(!member) return message.channel.send(xdemb)
-        
-      if(!member.kickable) 
+        .setColor("#00ff00")
+        .setTitle("Kick Command")
+        .addField("Description:", `Kick a member`, true)
+        .addField("Usage:", "!kick [user] [reason]", true)
+        .addField("Example:", "!kick @hackernub123 spam")
+
+    const member = message.mentions.members.first();
+    if (!member) return message.channel.send(xdemb)
+
+    if (!member.kickable)
         return message.channel.send("I cannot kick this user!");
-    
-  
-      
-      const reason = args.slice(1).join(' ');
-      if(!reason) {
+
+
+
+    const reason = args.slice(1).join(' ');
+    if (!reason) {
         res = "No reason given";
-      }
-      else {
+    } else {
         res = `${reason}`
-      }
-      
-      await member.kick(reason)
+    }
+
+    await member.kick(reason)
         .catch(error => message.reply(`Sorry, I couldn't kick because of : ${error}`));
-  
-        const kick = new Discord.MessageEmbed()
+
+    const kick = new Discord.MessageEmbed()
         .setColor("#00ff00")
         .setTitle(`Kick | ${member.user.tag}`)
         .addField("User", member, true)
@@ -36,12 +34,12 @@ module.exports.run = async (bot, message, args) => {
         .addField("Reason", res)
         .setTimestamp()
         .setFooter(member.id)
-    
-  
-        message.channel.send(kick)
-  
-      message.delete();
-    }
+
+
+    message.channel.send(kick)
+
+    message.delete();
+}
 
 module.exports.config = {
     name: "kick",
--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
-    
 
 
 
@@ -14,17 +13,18 @@ module.exports.run = async (bot, message, args) => {
 
 
 
- let embed = new Discord.MessageEmbed()
 
- .setColor('0x#ff0000')
- .setTitle('Server Information')
- .addField('Current Server', message.guild.name)
- .addField('Owner', message.guild.owner)
- .addField('Created Date', message.guild.createdAt)
- .addField('Member count', message.guild.memberCount)
- .setThumbnail(message.guild.iconURL)
- 
-message.channel.send(embed);
+    let embed = new Discord.MessageEmbed()
+
+        .setColor('0x#ff0000')
+        .setTitle('Server Information')
+        .addField('Current Server', message.guild.name)
+        .addField('Owner', message.guild.owner)
+        .addField('Created Date', message.guild.createdAt)
+        .addField('Member count', message.guild.memberCount)
+        .setThumbnail(message.guild.iconURL)
+
+    message.channel.send(embed);
 
 
 
