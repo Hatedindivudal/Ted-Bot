23a4c73eba5398706b138caa6a2b1b66f037ce46
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-07-08 01:55:12 -0300

    Create battle.js

---
+++ commands/battle.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js")
+module.exports.run = async (bot, message, args) => {
+
+
+
+
+const embed = new Discord.MessageEmbed()
+.setColor("RANDOM")
+.setTitle('**BATTLE!!**')
+.addField('test to see if the bot can send emojis in other servers other then where he got the emoji from', ':wasp:')
+message.channel.send(embed)
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
+
+module.exports.config = {
+    name: "battle",
+    description: "Bans a User",
+    usage: "-ban",
+    accessableby: "Admins",
+    aliases: []
+}
