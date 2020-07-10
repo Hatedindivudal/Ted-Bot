4f03c3ff6adbf87cdb05e30b89dad2b101531ad9
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-07-10 03:35:15 -0300

    Update test.123.js

--- commands/test.123.js
+++ commands/test.123.js
@@ -26,8 +26,29 @@ module.exports.run = async (bot, message, args) => {
 
 
 
+    let filter = m => m.author.id === message.author.id;
+    let q1 = new Discord.MessageCollector(message.channel, filter, {
+        max: 1
+    })
+    message.channel.send('Which Channel you want me to send');
 
-    message.guild.channel.updateOverwrite(channel.guild.roles.everyone, { SEND_MESSAGES: false });
+    q1.on('collect', async (message, col) => {
+        let channel = message.mentions.channels.first();
+
+        message.channel.send('What is the message you want to send in there?')
+        q1.stop();
+        let q2 = new Discord.MessageCollector(message.channel, filter, {
+            max: 1
+        })
+        q2.on('collect', async (message, col) => {
+            channel.send(message.content);
+            await message.react('😀');
+            message.channel.send(`Its working! go to ${channel} to check your message out!!`)
+            q2.stop();
+        })
+    })
+
+
 
 
 
