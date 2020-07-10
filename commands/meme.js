904fab0994105d4f28c334a264d50000522f378f
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-07-10 03:16:06 -0300

    Create meme.js

---
+++ commands/meme.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const randomPuppy = require('random-puppy');
+
+module.exports.run = async (bot, message, args) => {
+
+    const subReddits = [ "meme","memes", "dankmemes" ]
+    const random = subReddits[Math.floor(Math.random() * subReddits.length)];
+    const img = await randomPuppy(random);
+
+    const embed = new Discord.MessageEmbed()
+    .setImage(img)
+    .setTitle(`From /r/${random}`)
+    .setURL(`http://reddit.com/${random}`)
+
+    message.channel.send(embed);
+
+}
+
+module.exports.config = {
+    name: "meme",
+    description: "",
+    usage: ".",
+    accessableby: "Members",
+    aliases: []
+}
