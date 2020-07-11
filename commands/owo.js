21bc0c74ee89a6f412133696eedd2415c7cf0651
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-07-11 14:10:39 -0300

    ws

---
+++ commands/owo.js
@@ -0,0 +1,44 @@
+const Discord = require('discord.js');
+module.exports.run = async (bot, message, args) => {
+
+
+
+
+
+  if (message.content.toLowerCase() === '-test3'){
+      message.channel.send('hahaha fuck you retard im testing something').catch(err => message.reply(`Something went wrong... ${err}`));
+
+
+
+
+
+  
+
+
+
+  }
+
+
+
+
+
+
+
+
+  }
+
+
+
+
+
+    
+module.exports.config = {
+    name: "purge",
+    description: "Purges Specified amount can not go past 100",
+    usage: "-purge",
+    accessableby: "Members",
+    aliases: ['c', 'purge']
+
+
+
+}
