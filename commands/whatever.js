0995b296759097bce20040bb4d6d326f08b77dbe
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-07-22 18:43:39 -0300

    uhasndsdn

---
+++ commands/whatever.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const randomPuppy = require('random-puppy');
+
+module.exports.run = async (bot, message, args) => {
+
+    const subReddits = [ "BeansInThings" ]
+    const random = subReddits[Math.floor(Math.random() * subReddits.length)]
+    const img = await randomPuppy(random);
+
+    const embed = new Discord.MessageEmbed()
+    .setImage(img)
+    .setTitle(`From /r/${random}`)
+    .setURL(`http://reddit.com/${random}`)
+
+    message.channel.send(embed);
+
+}
+
+module.exports.config = {
+    name: "bean",
+    description: "Pulls random memes from r/meme r/memes and r/dankmemes Some memes will not load i am not sure why it is an issue",
+    usage: "-",
+    accessableby: "Members",
+    aliases: []
+}
