e9cf235d04f95830d7c9e3fd81b74e46f6d30c62
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-07-24 01:57:57 -0300

    Update cool.js

--- commands/cool.js
+++ commands/cool.js
@@ -4,9 +4,9 @@ module.exports.run = async (bot, message, args) => {
     const randomnumber =  Math.floor(Math.random() * 100 ) + 1;
 
     const embed = new Discord.MessageEmbed()
-    .setColor("RANDOM")
-    .setTitle("How cool are you?")
-    .addField('You are', `${random}% cool!`)
+    .setColor('0x#ff0000')
+    .setTitle("testing")
+    .addField('You are', `testing a command`)
     message.channel.send(embed);
     
 
