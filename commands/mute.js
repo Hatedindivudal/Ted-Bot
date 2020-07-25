e8643144d932af3fb3685df87ad36b2b46fdc492
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-07-24 22:28:56 -0300

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -3,57 +3,36 @@ const Discord = require("discord.js");
 const ms = require('ms');
 
 module.exports.run = async (bot, message, args) => {
- 
-// This defines member as the first person that get's mentioned in the message.
-let member = message.guild.member(message.mentions.users.first());
+    if(message.member.hasPermission('MANAGE_MESSAGES')) {
+        var member = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
+        if(!member) return message.reply('Please Provide a Member to TempMute.')
 
-// If there is no member defined this will say that it could not find anyone by that name.
-if(!member) return message.channel.send("I couldn't find a user by that name.");
+        let mainrole = message.guild.roles.cache.find(role => role.name === "Member");
+        let role = message.guild.roles.cache.find(role => role.name === "Muted");
 
-// This defines the main role and the muted role (this works from a config file, so you need to replace
-// botconfig.memberrole and botconfig.mutedrole with the names of the roles.)
-let mainrole = message.guild.roles.cache.find(role => role.name === "Members");
-let muterole = message.guild.roles.cache.find(role => role.name === "Muted");
+        if (!role) return message.reply("Couldn't find the 'muted' role.")
 
-// If either role is missing it will send a reply to the user trying to mute someone.
-if(!muterole) return message.reply("Couldn't find the mute role.")
-if(!mainrole) return message.reply("Couldn't find the default / member role.")
+        let time = args[1];
+        if (!time) {
+            return message.reply("You didnt specify a time!");
+        }
 
-// This makes it so that the second argument is the time.
-let time = args[2];
-// If you didn't specify a time in the second argument:
-if(!time) {
+        member.roles.remove(mainrole.id)
+        member.roles.add(role.id);
 
-    // Reply with this message.
-    message.reply("You need to specify a time in the second argument!")
-   
-    // Stop.
-    return;
-}
+        message.channel.send(`@${member.user.tag} has now been muted for ${ms(ms(time))}`)
 
-// Remove the main role and adds the muted role.
-member.roles.remove(mainrole.id);
-member.roles.add(muterole.id);
+        setTimeout( function () {
+            member.roles.add(mainrole.id)
+            member.roles.remove(role.id);
+            message.channel.send(`@${member.user.tag} has been unmuted.`)
+        }, ms(time));
 
-// Sends a message mentioning the person who got muted and how long they are muted for.
-message.channel.send(`<@${member.user.id}> has now been muted for ${ms(ms(time))}`);
-
-// This is the time function. When the time is done:
-setTimeout(function() {
-
-    // Add the main role back and remove the muted role.
-    member.roles.add(mainrole.id);
-    member.roles.remove(muterole.id);
-
-    // Sends a message mentioning the person saying that they have been unmuted.
-    message.channel.send(`<@${member.user.id}> has been unmuted.`)
-   
-}, ms(time));
+    } else {
+        return message.channel.send('You dont have perms.')
+    }
 
 }
-
-
-
 
 
 
