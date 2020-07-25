9584ba795b3202fae64408a08326b3a2dadf50fc
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-07-25 01:10:32 -0300

    asdwasd

---
+++ commands/troll.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+module.exports.run = async (bot, message, args) => {
+
+    message.channel.send('-Dove#2807')
+
+
+}
+
+module.exports.config = {
+    name: "Dove#2847",
+    description: "Says hi",
+    usage: "-hi",
+    accessableby: "Members",
+    aliases: ['hi', 'hi']
+
+
+
+}
