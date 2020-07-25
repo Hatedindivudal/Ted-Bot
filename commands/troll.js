1e6a5641c26a18d0641dc4f41cadd55ce2c47f9b
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-07-25 01:26:48 -0300

    Update troll.js

--- commands/troll.js
+++ commands/troll.js
@@ -1,13 +1,13 @@
 const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
 
-    message.channel.send('-Dove#2807')
+    message.channel.send('-@Dove#2807')
 
 
 }
 
 module.exports.config = {
-    name: "Dove#2807",
+    name: "@Dove#2807",
     description: "Says hi",
     usage: "-hi",
     accessableby: "Members",
