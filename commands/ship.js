a1e6d5220128f25a7635942ec44cbab156e45099
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-07-26 18:50:26 -0300

    Update ship.js

--- commands/ship.js
+++ commands/ship.js
@@ -8,17 +8,32 @@ if(!member) return message.channel.send('Please mention a valid user.')
 
 var absiembed = new Discord.MessageEmbed()
 .setAuthor('How compatible are you?')
-.setDescription(`You and ${member} are 0% Compatible!`)
+.setDescription(`You and ${member} are 0% Compatible!`);
+
+var doveembed = new Discord.MessageEmbed()
+.setAuthor('haha')
+.setDescription('Dove is gay')
 if(message.author.id == 483262780376481801){
     return message.channel.send(absiembed)
   }
   if(member.user.id === '483262780376481801'){
 return message.channel.send(absiembed)
   }
+
+  if(message.author.id == 249285155481452544){
+    return message.channel.send(doveembed)
+  }
+
+
+  if(member.user.id === '249285155481452544'){
+    return message.channel.send(doveembed)
+      }
 const Shipembed = new Discord.MessageEmbed()
 .setAuthor('How compatible are you?')
 .setDescription(`You and ${member} are ${ship}% compatible! `)
-message.channel.send(Shipembed)
+message.channel.send(Shipembed);
+
+
 
 
 
