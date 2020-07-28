76196866c465ae07f81dba577b40ce3c199c2c7a
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-07-27 22:15:54 -0300

    Update howgay.js

--- commands/howgay.js
+++ commands/howgay.js
@@ -1,23 +1,16 @@
 const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
    
-    const number123 = Math.floor(Math.random() * 100 ) + 1;
-    const member = message.mentions.members.first()
+const ship = Math.floor(Math.random() * 100 ) + 1;
+const member = message.mentions.members.first();
 
-    const gayembed = new Discord.MessageEmbed()
-    .setAuthor(':rainbow_flag: The Gay Machine :rainbow_flag: ')
-    .setDescription(`${member} is ${number123}% Lol retard `)
-   
-    
-    const yourself = new Discord.MessageEmbed()
-    .setAuthor('The Gay Machine')
-    .setDescription(`:rainbow_flag: You are ${number123}% gay :rainbow_flag: `)
+if(!member) return message.channel.send('Please mention a valid user.')
 
-    if(!member){
-     return message.channel.send(yourself)
-     
-    }
-    if(member) return message.channel.send(gayembed)
+const Shipembed = new Discord.MessageEmbed()
+.setColor("RANDOM")
+.setAuthor(':rainbow_flag: The Gay Machine :rainbow_flag: ')
+.setDescription(`:rainbow_flag: ${member} is ${ship}% Gay! :rainbow_flag:  `)
+message.channel.send(Shipembed);
     
     
    
