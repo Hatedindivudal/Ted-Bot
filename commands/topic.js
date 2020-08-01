96b15e94ee0dcb5da7bac900e83378b4f3a7d45a
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-07-31 23:15:34 -0300

    Update topic.js

--- commands/topic.js
+++ commands/topic.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
     module.exports.run = async (bot, message, args) => {
     
         let aa = ['you should get a life', 'get a life loser a life', 'how are you']
-        const a = aa[Math.floor(Math.random()*options.length)];
+        const a = aa[Math.floor(Math.random()*aa.length)];
 message.channel.send(a)
     
     }
