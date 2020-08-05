f88c74221a11ca533cbdb6527a72d3ee5b4c6ba0
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-08-05 15:08:01 -0300

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -2,7 +2,10 @@ const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
    
 message.channel.send('https://discord.com/oauth2/authorize?client_id=704153409309376562&scope=bot&permissions=8')
-    
+    const embed = new Discord.MessageEmbed()
+    .setAuthor('Thanks for inviting my bot!')
+    .addField("Click on this", "Add DiscordBot to your server! [Click here]( https://discord.com/oauth2/authorize?client_id=704153409309376562&scope=bot&permissions=8)")
+
     
    
     
