364282a4843fbc43f5eee19784fbb4efc416fb00
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-08-23 00:58:20 -0300

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -9,7 +9,7 @@ module.exports.run = async (bot, message, args) => {
         let mainrole = message.guild.roles.cache.find(role => role.name === "Member");
         let role = message.guild.roles.cache.find(role => role.name === "Muted");
 
-        if (user.roles.highest.position >= message.member.roles.highest.position) return message.channel.send('That user has a higher role then you!')
+        if (message.member.roles.highest.position >= member.roles.highest.position) return message.channel.send('That user has a higher role then you!')
         
 
         if(!member.roles.cache.find(r => r.name === "Muted")) {
