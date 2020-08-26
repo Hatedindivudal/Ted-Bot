70e5c6f6d8ee01239004d9bbba69eef06b69d70b
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2020-08-26 00:42:59 -0300

    Update roast.js

--- commands/roast.js
+++ commands/roast.js
@@ -86,7 +86,7 @@ var roast = [
     const roasts = roast[Math.floor(Math.random() * roast.length)];
     const embed = new Discord.MessageEmbed()
         .setColor(0x00A2E8)
-        .setDescription(member.username + ", " + roasts);
+        .setDescription(member + ", " + roasts);
     message.channel.send(embed)
   }
 
