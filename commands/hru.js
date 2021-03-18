f806e00982ce07873cbba1d9e186985ac14f79ad
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-03-18 20:25:32 -0300

    Create hru.js

---
+++ commands/hru.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+module.exports.run = async (bot, message, args) => {
+
+
+
+
+message.channel.send("Good!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+module.exports.config = {
+    name: "hru",
+    description: "Kicks a user",
+    usage: "-kick",
+    accessableby: "Admins, ",
+    aliases: []
+}
+
