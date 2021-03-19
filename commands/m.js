9c976bd4446c54094cbe4d128a604616d8ff9cfd
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-03-18 21:35:53 -0300

    Create m.js

---
+++ commands/m.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+module.exports.run = async (bot, message, args) => {
+
+const { inspect } = require('util');
+
+if (message.author.id !== 'ownerID') return;
+    
+let evaled;
+try {
+  evaled = await eval(args.join(' '));
+  message.channel.send(inspect(evaled));
+  console.log(inspect(evaled));
+}
+catch (error) {
+  console.error(error);
+  message.reply('there was an error during evaluation.');
+}
+}
+module.exports.config = {
+    name: "eval",
+    description: "Purges Specified amount can not go past 100",
+    usage: "-purge",
+    accessableby: "Members",
+    aliases: ['c', 'purge']
+}
