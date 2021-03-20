44a89282c7a4faf0d344b54b68baff286fa67cee
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-03-20 01:40:06 -0300

    Update topic.js

--- commands/topic.js
+++ commands/topic.js
@@ -1,7 +1,18 @@
 const Discord = require('discord.js');
     module.exports.run = async (bot, message, args) => {
     
-        let aa = ['If you could have any superpower what would it be?', 'lose some weight fatass', 'take a picture it will last longer',]
+        let aa = ["if you could have any superpower what would it be?",
+        "What is the worst advice you have given?",
+        "What is one thing you should never say at a wedding?",
+        "What is the weirdest food combination you’ve ever tried?",
+        "Did you have an imaginary friend? What was his/her name?",
+        "Have you ever had a dream where everyone was in their underwear?",
+        "If you could do anything illegal without getting caught, what would you do?"
+        
+        
+    ]
+
+        
      const a = aa[Math.floor(Math.random()*aa.length)];
 message.channel.send(a).then().catch(console.error);
     
