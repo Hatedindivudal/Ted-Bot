a2f313ad13f3af655642f041d555ac99cbefcd44
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-03-20 01:01:09 -0300

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,60 +1,49 @@
 const Discord = require("discord.js")
 module.exports.run = async (bot, message, args) => {
-    const member = message.mentions.members.first();
-
-    const xdemb = new Discord.MessageEmbed()
-    .setColor("#00ff00")
-    .setTitle("Ban Command")
-    .addField("Description:", `Ban a member`, true)
-    .addField("Usage:", `-ban [user] [reason]`, true)
-    .addField("Example:", `-ban @Randomexample#7183`);
-
-    
-
     if (!message.member.hasPermission("BAN_MEMBERS")) {
         return message.reply("Missing Permissions!").then(m => m.delete(5000));
     }
-   
-    if(!member) return message.channel.send(xdemb)
-    if(!member.bannable) return message.channel.send("I can't ban this user!");
-   
+  
 
-    
+  
+  const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
 
+  // Final Check
+  if(!member) return message.channel.send('member not found');
+  if(member.id === message.author.id) return message.channel.send("You can't ban your self");
+  if(!member.bannable) return message.channel.send('not bannable');
 
+  // Reason
+  let reason = args.slice(1).join(" ");
+  if(!reason) reason = "no reason";
 
-    if(member.id === message.author.id) return message.channel.send("You can't ban your self")
-
-   
-    const reason = args.slice(1).join(" ");
+  // dmMessage embed
     let embed2 = new Discord.MessageEmbed()
     .setColor('RANDOM')
     .setTitle('You have been banned!')
     .addField(`You have been banned from ${message.guild.name}`, ` ${reason}`);
-    if(member)return member.send(embed2);
+    
+    try {
+    member.send(embed2)
+} catch(e) {
+console.log(e)
+}
 
-
-
-    if(!reason) {
-        res = "No reason given";
-    } else {
-        res = reason
-    }
-
-    await member.ban(reason).catch(error => message.channel.send(`Sorry, I coldn't ban because of: ${error}`));
+    await member.ban({reason: reason}).catch(error => message.channel.send(`Sorry, I coldn't ban because of: ${error}`));
 
     const bean = new Discord.MessageEmbed()
     .setColor("#00ff00")
     .setTitle(`Ban | ${member.user.tag}`)
     .addField("User", member, true)
-    .addField("Moderator", message.author, true)
-    .addField("Reason", res)
-    
+    .addField("Moderator", message.member, true)
+    .addField("Reason", reason)
+    .setTimestamp()
 
     message.channel.send(bean);
-
-   
 }
+
+
+
 
 module.exports.config = {
     name: "ban",
