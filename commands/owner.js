73749a6c7d87e6b98b24d9abe8438effd58666b1
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-03-20 21:32:11 -0300

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const ms = require('ms');
 
 module.exports.run = async (bot, message, args) => {
-if(message.guild.owner) return; 
+if(!message.guild.owner) return; 
 
 message.channel.send('hi')
 
