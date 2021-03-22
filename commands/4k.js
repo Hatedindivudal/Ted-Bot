243aaac4f4e9510a63b5bd9b7d97cafbace17259
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-03-22 02:06:01 -0300

    swasd

---
+++ commands/4k.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+    if(!message.channel.nsfw) return;
+
+
+
+const Discord = require("discord.js");
+const NSFW = require("discord-nsfw");
+const nsfw = new NSFW();
+
+const image = await nsfw.pussy();
+const embed = new Discord.MessageEmbed()
+    .setTitle(`Pussy Image`)
+    .setColor("GREEN")
+    .setImage(image);
+message.channel.send(embed);
+
+
+}
+
+module.exports.config = {
+    name: "pussy",
+    description: "",
+    usage: ".",
+    accessableby: "Members",
+    aliases: []
+}
