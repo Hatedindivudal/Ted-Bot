c0a038b7c08a93a1fc53cb5d94a01b929fed2ccf
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-03-22 02:14:34 -0300

    Create boobs.js

---
+++ commands/boobs.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+    if(!message.channel.nsfw) return;
+
+
+
+const Discord = require("discord.js");
+const NSFW = require("discord-nsfw");
+const nsfw = new NSFW();
+
+const image = await nsfw.boobs();
+const embed = new Discord.MessageEmbed()
+    .setTitle(`Here you go!`)
+    .setColor("GREEN")
+    .setImage(image);
+message.channel.send(embed);
+
+
+}
+
+module.exports.config = {
+    name: "boobs",
+    description: "",
+    usage: ".",
+    accessableby: "Members",
+    aliases: []
+}
