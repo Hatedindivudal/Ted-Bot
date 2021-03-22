faa71e9a6a3bb80bb22d641f73be47cb3d5d1234
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-03-22 02:44:36 -0300

    Create Kitsune.js

---
+++ commands/Kitsune.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+    if(!message.channel.nsfw) return;
+
+
+
+const Discord = require("discord.js");
+const NSFW = require("discord-nsfw");
+const nsfw = new NSFW();
+
+const image = await nsfw.kitsune();
+const embed = new Discord.MessageEmbed()
+    .setTitle(`Here you go!`)
+    .setColor("GREEN")
+    .setImage(image);
+message.channel.send(embed);
+
+
+}
+
+module.exports.config = {
+    name: "kitsune",
+    description: "",
+    usage: ".",
+    accessableby: "Members",
+    aliases: []
+}
