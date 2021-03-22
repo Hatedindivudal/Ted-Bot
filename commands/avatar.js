d2565f8964b0154902de9d76be0b91d5aa39507c
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-03-21 21:14:07 -0300

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -3,7 +3,7 @@ module.exports.run = async (bot, message, args) => {
     const member = message.mentions.members.first();
 
     if(member){
-       return message.member.displayAvatarURL();
+       return member.displayAvatarURL();
     }
    
 
