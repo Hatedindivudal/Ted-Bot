d8a3fc5152a512ca54ac858413bf9eb0d23196b9
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-03-21 21:47:59 -0300

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js")
 module.exports.run = async (bot, message, args) => {
-    const member = message.mentions.members.first();
+    
 
     let member = message.mentions.users.first() || message.author
 
