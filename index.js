5896cf8b4c793548b684713e35ae430123fb345b
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-03-27 12:47:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-bot.events = new Discord.Collection();
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://Hated:7reiRRZ32Q7FF5qy@cluster0.gpkqk.mongodb.net/Data', { useNewUrlParser: true, useUnifiedTopology: true})
 
 const profileModel = require("./models/profileSchema");
 mongoose.set('useCreateIndex', true);
 
+module.exports = async (bot, discord, member) => {
+  let profile = await profileModel.create({
+    userID: member.id,
+    serverID: member.guild.id,
+    coins: 1000,
+    bank: 0,
+  });
+  profile.save();
+};
 
 const prefix = '-';
 
 const embed = new Discord.MessageEmbed();
 
 
+
+
+
 const cheerio = require('cheerio')
 
-const request = require('request');
+const request = require('request')
 
 const validateFlag = f => f === 'true' || f === 'false' || f === 'null';
 
@@ -26,6 +37,24 @@ const IGNORED = new Set([
   const fs = require('fs');
   bot.commands = new Discord.Collection();
 bot.aliases = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+
+    if(err) console.log(err)
+
+    let jsfile = files.filter(f => f.split(".").pop() === "js") 
+    if(jsfile.length <= 0) {
+         return console.log("[LOGS] Couldn't Find Commands!");
+    }
+
+    jsfile.forEach((f, i) => {
+        let pull = require(`./commands/${f}`);
+        bot.commands.set(pull.config.name, pull);  
+        pull.config.aliases.forEach(alias => {
+            bot.aliases.set(alias, pull.config.name)
+        });
+    });
+});
 
 const ms = require('ms');
 
@@ -57,6 +86,21 @@ bot.on("message", async message => {
 
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
+  let profileData;
+  try {
+    profileData = await profileModel.findOne({ userID: message.author.id });
+    if (!profileData) {
+      let profile = await profileModel.create({
+        userID: message.author.id,
+        serverID: message.guild.id,
+        coins: 1000,
+        bank: 0,
+      });
+      profile.save();
+    }
+  } catch (err) {
+    console.log(err);
+  }
   
   
 
