cfe0c4b0687dd2913a02126bd866adcf9aa4332b
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-03-28 15:14:20 -0300

    s

---
+++ events/test.js
@@ -0,0 +1,27 @@
+module.exports = bot => { 
+
+let profile = await profileModel.create({
+    userID: member.id,
+    serverID: member.guild.id,
+    coins: 1000,
+    bank: 0,
+  });
+  profile.save();
+
+
+  let profileData;
+  try {
+    profileData = await profileModel.findOne({ userID: message.author.id });
+    if (!profileData) {
+      let profile = await profileModel.create({
+        userID: message.author.id,
+        serverID: message.guild.id,
+        coins: 1000,
+        bank: 0,
+      });
+      profile.save();
+    }
+  } catch (err) {
+    console.log(err);
+  } 
+};
