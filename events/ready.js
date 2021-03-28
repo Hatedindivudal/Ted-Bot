1dd73d8780b074666b7af82ea9e57f8d6f9bc8d9
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-03-28 14:47:30 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,6 @@ const Discord = require("discord.js")
 
 module.exports = bot => { 
    
-    bot.user.setPresence({ activity: { name: 'In devolpment!' }, status: 'dnd' })
+    bot.user.setPresence({ activity: { name: 'Hitler.' }, status: 'dnd' })
     .catch(console.error);
 }
