33055e8f5b5c7d79979acfa82ad939aadf6a37f9
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-03-28 15:38:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://Hated:7reiRRZ32Q7FF5qy@cluster0.gpkqk.mongodb.net/Data', { useNewUrlParser: true, useUnifiedTopology: true})
-
+mongoose.set('useCreateIndex', true);
 
 require("./util/eventHandler")(bot)
 
