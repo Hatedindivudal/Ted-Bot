79b7f6eef950cde2eea3cbf4a83ab2324da2df35
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-04-01 15:06:38 -0300

    Update deposit.js

---
+++ commands/deposit.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+module.exports.run = async (bot, message, args) => {
+  
+
+    
+
+
+}
+
+module.exports.config = {
+    name: "deposit",
+    description: "Says hi",
+    usage: "-hi",
+    accessableby: "Members",
+    aliases: ['hi', 'hi']
+
+
+
+}
