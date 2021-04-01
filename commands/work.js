8fd8962c13af6afefbdccedadfcbadfbae3942f0
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-03-31 23:35:45 -0300

    Create work.js

---
+++ commands/work.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args, profileData) => {
+
+
+
+    const randomNumber = Math.floor(Math.random() * 500) + 1;
+    const response = await profileModel.findOneAndUpdate(
+      {
+        userID: message.author.id,
+      },
+      {
+        $inc: {
+          coins: randomNumber,
+        },
+    }
+    );
+    return message.channel.send(`${message.author.username}, you have become a hitman and received ${randomNumber} **coins**`);
+  },
+
+
+  
+
+
+
+module.exports.config = {
+    name: "bal",
+    description: "",
+    usage: ".",
+    accessableby: "Members",
+    aliases: []
+}
