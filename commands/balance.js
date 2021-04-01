d1ee5af023370286448588dccf48127af3574b07
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-03-31 23:24:58 -0300

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -1,10 +1,10 @@
 const Discord = require("discord.js")
 module.exports.run = async (bot, message, args, profileData) => {
     
-}
+
 
 message.channel.send(`Your wallet balance is currently ${profileData.coins}`)
-
+}
 
 
 module.exports.config = {
