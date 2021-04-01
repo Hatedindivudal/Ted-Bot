d5c27f7db5c01a9650aeaf73ada953a446734376
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-04-01 15:37:11 -0300

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args, profileData) => {
 
 const balanceembed = new Discord.MessageEmbed()
 .setTitle(`${message.member}`)
-.addField(`Pocket: ${profileData.coins}`)
+.addField(`Pocket: ${profileData.coins, true}`)
 message.channel.send(balanceembed);
 
 
