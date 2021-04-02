e85542c5d0d84f4188d78ecdd5a7d2f404031338
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-04-02 02:45:27 -0300

    Create withdraw.js

---
+++ commands/withdraw.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js');
+module.exports.run = async (bot, message, args, profileData) => {
+    const profileModel = require("../models/profileSchema");
+
+    
+        const amount = args[0];
+        if (amount % 1 != 0 || amount <= 0) return message.channel.send("That's not a number!");
+    
+        try {
+          if (amount > profileData.bank) return message.channel.send(`You dont have that many coins in your bank!`);
+    
+          await profileModel.findOneAndUpdate(
+            {
+              userID: message.author.id,
+            },
+            {
+              $inc: {
+                coins: amount,
+                bank: -amount,
+              },
+            }
+          );
+    
+          return message.channel.send(`You withdrew ${amount} of coins into your wallet`);
+        } catch (err) {
+          console.log(err);
+        }
+      },
+    
+
+module.exports.config = {
+    name: "withdraw",
+    description: "Says hi",
+    usage: "-hi",
+    accessableby: "Members",
+    aliases: ['hi', 'hi']
+
+
+
+}
