aca50b281fcef0301b2693bb5d48ec28c4b722db
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-04-05 21:33:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -66,30 +66,6 @@ fs.readdir("./commands/", (err, files) => {
 
 
 
-const cooldowns = new Map();
-if(!cooldowns.has(command.name)){
-    cooldowns.set(command.name, new Discord.Collection());
-}
-
-const current_time = Date.now();
-const time_stamps = cooldowns.get(command.name);
-const cooldown_amount = (command.cooldown) * 1000;
-  //If time_stamps has a key with the author's id then check the expiration time to send a message to a user.
-  if(time_stamps.has(message.author.id)){
-    const expiration_time = time_stamps.get(message.author.id) + cooldown_amount;
-
-    if(current_time < expiration_time){
-        const time_left = (expiration_time - current_time) / 1000;
-
-        return message.reply(`Please wait ${time_left.toFixed(1)}`);
-    }
-}
-
-//If the author's id is not in time_stamps then add them with the current time.
-time_stamps.set(message.author.id, current_time);
-//Delete the user's id once the cooldown is over.
-setTimeout(() => time_stamps.delete(message.author.id), cooldown_amount);
-
 
 const ms = require('ms');
 
@@ -138,6 +114,30 @@ bot.on("message", async message => {
     if(command) command.run(bot,message,args, profileData);
 
 
+    const cooldowns = new Map();
+    if(!cooldowns.has(command.name)){
+        cooldowns.set(command.name, new Discord.Collection());
+    }
+    
+    const current_time = Date.now();
+    const time_stamps = cooldowns.get(command.name);
+    const cooldown_amount = (command.cooldown) * 1000;
+      //If time_stamps has a key with the author's id then check the expiration time to send a message to a user.
+      if(time_stamps.has(message.author.id)){
+        const expiration_time = time_stamps.get(message.author.id) + cooldown_amount;
+    
+        if(current_time < expiration_time){
+            const time_left = (expiration_time - current_time) / 1000;
+    
+            return message.reply(`Please wait ${time_left.toFixed(1)}`);
+        }
+    }
+    
+    //If the author's id is not in time_stamps then add them with the current time.
+    time_stamps.set(message.author.id, current_time);
+    //Delete the user's id once the cooldown is over.
+    setTimeout(() => time_stamps.delete(message.author.id), cooldown_amount);
+    
     
 })
 
