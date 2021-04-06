2f81cb19d5c0bc2afd666057eea0e26909784863
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-04-06 18:31:45 -0300

    Update index.js

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ const fetch = require("node-fetch").default
 
 
 bot.on("message", async message => {
-    if (message.channel.type === "dm") return;
+    if(message.author.bot || message.channel.type === "dm") return;
     if (message.channel.id === "829051130980859904"){
         fetch(`https://api.monkedev.com/fun/chat?msg=${message.content}&uid=${message.author.id}&key=xz0Vo4KLSX6q3PoVFQH1BHisu`)
         .then(response => response.json())
