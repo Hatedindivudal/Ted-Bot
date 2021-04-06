4e34186120a261e86ed29648dec4459398699021
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-04-05 21:41:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -115,24 +115,25 @@ bot.on("message", async message => {
 
 
     const cooldowns = new Map();
-    if(!cooldowns.has(command.name)){
-        cooldowns.set(command.name, new Discord.Collection());
+    if(!cooldowns.has(commands.name)){
+        cooldowns.set(commands.name, new Discord.Collection());
     }
-    
+
     const current_time = Date.now();
-    const time_stamps = cooldowns.get(command.name);
-    const cooldown_amount = (command.cooldown) * 1000;
-      //If time_stamps has a key with the author's id then check the expiration time to send a message to a user.
-      if(time_stamps.has(message.author.id)){
+    const time_stamps = cooldowns.get(commands.name);
+    const cooldown_amount = (commands.cooldown) * 1000;
+
+    //If time_stamps has a key with the author's id then check the expiration time to send a message to a user.
+    if(time_stamps.has(message.author.id)){
         const expiration_time = time_stamps.get(message.author.id) + cooldown_amount;
-    
+
         if(current_time < expiration_time){
             const time_left = (expiration_time - current_time) / 1000;
-    
-            return message.reply(`Please wait ${time_left.toFixed(1)}`);
+
+            return message.reply(`Please wait ${time_left.toFixed(1)}}`);
         }
     }
-    
+
     //If the author's id is not in time_stamps then add them with the current time.
     time_stamps.set(message.author.id, current_time);
     //Delete the user's id once the cooldown is over.
