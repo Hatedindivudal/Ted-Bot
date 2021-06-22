7f6ff29fe258f56a8239caa1a4a2014df7fe9ea5
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-06-22 20:54:24 -0300

    wasd

--- commands/help.js
+++ commands/help.js
@@ -1,47 +1,23 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    //We have to set a argument for the help command beacuse its going to have a seperate argument.
-    let helpArray = message.content.split(" ");
-    let helpArgs = helpArray.slice(1);
 
-    //Custom Help command by using the second argument.
-    if(helpArgs[0] === 'kick') {
-        return message.reply("Kick mentioned user")
+    if (!args.length) {
+        data.push('Here\'s a list of all my commands:');
+        data.push(commands.map(command => command.name).join(', '));
+        
+    
+        return message.author.send(data, { split: true })
+            .then(() => {
+                if (message.channel.type === 'dm') return;
+                message.reply('I\'ve sent you a DM with all my commands!');
+            })
+            .catch(error => {
+                console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
+                message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
+            });
     }
-
-    //Normal usage of (prefix)help without any args. (Shows all of the commands and you should set the commands yourself)
-    if(!helpArgs[0]) {
-        const embed1 = new Discord.MessageEmbed()
-        .addField('⚙ **Moderation**', ' `lock` `kick` `ban` `mute` `purge` `unban` `warn` `warns`  `unmute`')
-        .addField('👾 **Fun**', '`Meme` `Topic` `Roast` `Ship` `howcool` `randomnumber`')
-        .addField('🤖 **Misc**', '`invite` `ping`')
-      
-
-        message.channel.send(embed1);
-    } 
-
-    //Reads the moudle.exports.config (This line of code is on commands folder, each command will read automaticly) by the second argument (the command name) and shows the information of it.
-    if(helpArgs[0]) {
-        let command = helpArgs[0];
-
-        if(bot.commands.has(command)) {
-            
-            command = bot.commands.get(command);
-            var embed = new Discord.MessageEmbed()
-            .setAuthor(`${command.config.name} Command`)
-            .setDescription(`
-            - **Command's Description** __${command.config.description || "There is no Description for this command."}__
-            - **Command's Usage:** __${command.config.usage || "No Usage"}__
-            - **Command's Permissions:** __${command.config.accessableby || "Members"}__
-            - **Command's Aliases:** __${command.config.aliases || "No Aliases"}__
-            `)
-            .setColor('#2EFF00')
-
-        message.channel.send(embed);
-    }}
 }
-
 module.exports.config = {
     name: "help",
     description: "",
