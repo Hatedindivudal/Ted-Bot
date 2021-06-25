91fa45b92f04d6b7beb0eb804c69d4939c94e1bb
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-06-25 00:09:40 -0300

    wasd

---
+++ commands/play.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+module.exports.run = async (bot, message, args) => {
+const music = args.join(" ");
+    distube.play(message, music);
+
+    
+
+
+}
+
+module.exports.config = {
+    name: "play",
+    description: "Says hi",
+    usage: "-hi",
+    accessableby: "Members",
+    aliases: ['hi', 'hi']
+
+
+
+}
