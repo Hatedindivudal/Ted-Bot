1a8e8e3ec2c5954685c2eb67a79b58c78f0251ef
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-06-25 00:24:03 -0300

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
+    const DisTube = require('distube');
+
 const music = args.join(" ");
     distube.play(message, music);
 
