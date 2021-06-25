5606a85300d5652d2ee3fe063b65af91197d1071
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-06-25 00:34:29 -0300

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
 
-    distube.stop(message);
+    bot.distube.stop(message);
     message.channel.send("Stopped the music!");
 
 
