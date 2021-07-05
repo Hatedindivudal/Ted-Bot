5779729d9eb680869981d29f5cce545d73d50f16
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-07-05 18:29:18 -0300

    Update iplookup.js

--- commands/iplookup.js
+++ commands/iplookup.js
@@ -3,16 +3,24 @@ const ms = require('ms');
 
 module.exports.run = async (bot, message, args) => {
     const fetch = require("node-fetch").default
-    fetch(`http://ipwhois.app/json/${message.content}?callback=getIPinfo`)
-    .then(response => response.json())
-    .then(data =>{
-
-        message.reply(data.response)
-
-    })
+ 
+    
 
 
+   
+        fetch(`http://ipwhois.app/json/${message.content}`)
+        .then(response => response.json())
+        .then(data =>{
 
+            message.reply(data.response)
+
+        })
+            .catch(() =>{
+message.channel.send('Im Broken!')
+            })
+
+        
+    
 
 
     
