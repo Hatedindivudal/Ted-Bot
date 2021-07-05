50705e963f041df74b8ea3ef243966cf14673594
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-07-05 17:55:05 -0300

    Update iplookup.js

--- commands/iplookup.js
+++ commands/iplookup.js
@@ -3,7 +3,7 @@ const ms = require('ms');
 
 module.exports.run = async (bot, message, args) => {
     const fetch = require("node-fetch").default
-    fetch(`http://ip-api.com/${message.content}`)
+    fetch(`http://ipwhois.app/json/${message.content}`)
     .then(response => response.json())
     .then(data =>{
 
