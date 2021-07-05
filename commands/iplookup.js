44eeea4b954198fdd6a1e81b396aaab082bdae56
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-07-05 17:28:21 -0300

    wasd

---
+++ commands/iplookup.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const ms = require('ms');
+
+module.exports.run = async (bot, message, args) => {
+    const fetch = require("node-fetch").default
+    fetch(`http://ip-api.com/json/${message.content}`)
+    .then(response => response.json())
+    .then(data =>{
+
+        message.reply(data.response)
+
+    })
+
+
+
+
+
+    
+
+
+
+
+
+}
+
+module.exports.config = {
+    name: "lookup",
+    description: "",
+    usage: ".ping",
+    accessableby: "Members",
+    aliases: []
+}
