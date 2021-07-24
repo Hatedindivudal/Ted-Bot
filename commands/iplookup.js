fe348da27d45b4556a8a14577dac28fe50354a87
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-07-24 06:24:41 -0300

    Update iplookup.js

--- commands/iplookup.js
+++ commands/iplookup.js
@@ -1,26 +1,24 @@
 const Discord = require('discord.js');
-module.exports.run = async (bot, message, args) => {
 const querystring = require('querystring');
-client.on('message', async message => {
-    if (command === '-lookup') {
-        if (!args.length) {
-            return message.channel.send('You need to supply a search term!');
-        }
-
-        const query = querystring.stringify({ term: args.join(' ') });
-
-        const { list } = await fetch(`http://ip-api.com/json/?${query}`)
-            .then(response => response.json());
+module.exports.run = async(bot, message, args) => {
+if (command === '-lookup') { //if command === "-
+    if (!args.length) {
+        return message.channel.send('You need to supply a search term!');
     }
-});
+
+    const query = querystring.stringify({ term: args.join(' ') });
+
+    const { list } = await fetch(`http://ip-api.com/json/?${query}`)
+        .then(response => response.json());
 }
+};
 
 module.exports.config = {
     name: "lookup",
     description: "Sends a bot invite link nothing more.",
     usage: "-invite",
     accessableby: "Members",
-    aliases: ['',]
+    aliases: ['', ]
 
 
 
