f100ea10f759c9969941a396bb33ad94cde57329
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-07-24 01:34:50 -0300

    Update iplookup.js

--- commands/iplookup.js
+++ commands/iplookup.js
@@ -10,7 +10,7 @@ const axios = require('axios')
    
         axios.get(`http://ip-api.com/json/${args}`)
         .then((res) => {
-          console.log(`RES:`, res.data)
+          message.channel.send(`RES:`, res.data)
         })
         .catch((err) => {
             message.channel.send(err)
