feee632a4e96c26c094d1bac93781a124b32de1c
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-07-24 06:06:54 -0300

    Update iplookup.js

--- commands/iplookup.js
+++ commands/iplookup.js
@@ -1,7 +1,8 @@
 const Discord = require("discord.js");
 const axios = require('axios');
-const api = `http://ip-api.com/json/${args}`;
 module.exports.run = async(bot, message, args) => {
+        const api = `http://ip-api.com/json/${args}`;
+
         const axios = require('axios')
         axios.get(api)
             .then(response => {
