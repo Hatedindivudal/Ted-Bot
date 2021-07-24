98760a2e2f1b1f211172b89db4aa810f14557c68
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-07-23 23:42:57 -0300

    Create skip.js

---
+++ commands/skip.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+module.exports.run = async (bot, message, args) => {
+    let queue = await bot.distube.getQueue(message);
+
+    if(queue){
+        bot.distube.skip(message);
+        message.channel.send("Skipped!");
+    }
+    if(!message.member.voice.channel) return message.channel.send('You must be in a voice channel to use this command!')
+
+   
+
+
+}
+
+module.exports.config = {
+    name: "stop",
+    description: "Says hi",
+    usage: "-hi",
+    accessableby: "Members",
+    aliases: ['hi', 'hi']
+
+
+
+}
