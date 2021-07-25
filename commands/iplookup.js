fd5cfea847a2a476b5efc3b607afec6b06da0f48
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-07-24 22:49:25 -0300

    Update iplookup.js

--- commands/iplookup.js
+++ commands/iplookup.js
@@ -3,15 +3,14 @@ const querystring = require('querystring');
 module.exports.run = async(bot, message, args, ) => {
 
 const axios = require("axios")
-    axios.get(`http://ip-api.com/json/${args}`)
-        .then((res) => {
-            var fag = JSON.stringify(res.data);
-            const embed = new Discord.MessageEmbed()
-            .addField('Country',  `${fag.country}`)
-            message.channel.send(embed)
-          .catch(err => message.reply(`Something went wrong... ${err}`));
-        })
-       
+axios.get(`http://ip-api.com/json/${text}`)
+.then((res) => {
+    const embed = new Discord.MessageEmbed()
+    .addField('Country',  `${JSON.stringify(res.data.country)}`)
+    
+    message.channel.send(embed)
+  .catch(err => message.reply(`Something went wrong... ${err}`));
+})
     }
 module.exports.config = {
     name: "lookup",
