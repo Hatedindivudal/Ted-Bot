5962ce1b6d858d4b4e117c5fd8ef14386db1a777
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-07-24 23:03:10 -0300

    Update iplookup.js

--- commands/iplookup.js
+++ commands/iplookup.js
@@ -6,8 +6,12 @@ const axios = require("axios")
 axios.get(`http://ip-api.com/json/${args}`)
 .then((res) => {
     const embed = new Discord.MessageEmbed()
-    .addField('Country',  `${JSON.stringify(res.data.country)}`)
-    
+    .addField('IP',  `${JSON.stringify(res.data.query).replace(/"/g, '').replace(/}/g, '')}`)
+    .addField('Country',  `${JSON.stringify(res.data.country).replace(/"/g, '').replace(/}/g, '')}`)
+    .addField('ISP', `${JSON.stringify(res.data.isp).replace(/"/g, '').replace(/}/g, '')}`)
+    .addField('City', `${JSON.stringify(res.data.city).replace(/"/g, '').replace(/}/g, '')}`)
+    .addField('Province/State', `${JSON.stringify(res.data.regionName).replace(/"/g, '').replace(/}/g, '')}`)
+
     message.channel.send(embed)
   .catch(err => message.reply(`Something went wrong... ${err}`));
 })
