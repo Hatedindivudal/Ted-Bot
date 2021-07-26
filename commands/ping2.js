2f0b387d78246927f0fac64670d18a8d946ea59d
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-07-26 05:07:11 -0300

    Create ping2.js

---
+++ commands/ping2.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+module.exports.run = async (bot, message, args) => {
+
+    message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+
+	}
+	module.exports.config = {
+		name: "ping2",
+		description: "",
+		usage: ".ping",
+		accessableby: "Members",
+		aliases: []
+	}
