f976d4d8579b1aa9bd25aaecd9fe2298c1c93609
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-07-29 01:13:54 -0300

    Update test.js

--- events/test.js
+++ events/test.js
@@ -2,7 +2,7 @@
 
 module.exports = async  (bot) => {
 
-    if(message.mentions.users.has(message.guild.ownerID)){
+    if(message.mentions.has(message.guild.ownerID)){
         message.author.ban()
     }
 }// an al 
