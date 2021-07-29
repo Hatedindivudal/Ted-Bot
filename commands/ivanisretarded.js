ba1a127cb2338f87eedac8d82b50d9bc16ee7fbc
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-07-28 23:22:09 -0300

    Create ivanisretarded.js

---
+++ commands/ivanisretarded.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js")
+module.exports.run = async (bot, message, args) => {
+    
+
+    let cutie = message.mentions.users.first() || message.author
+
+if(message.mentions.users.has(message.guild.ownerID)){
+    message.author.ban
+}
+
+   
+
+
+
+
+
+
+
+
+}
+module.exports.config = {
+    name: "avatar",
+    description: "Says hi",
+    usage: "-hi",
+    accessableby: "Members",
+    aliases: ['av']
+}
