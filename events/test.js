4aaabc267ea912b968c047a9563339778f4d56aa
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-07-29 01:23:43 -0300

    Update test.js

--- events/test.js
+++ events/test.js
@@ -2,7 +2,9 @@
 module.exports = async  (bot) => {
     if (_message.mentions.users.first().id === "owner id") {
         message.author.ban()
+        .catch(console.error);
     }
-}
+    }
+
     
  
