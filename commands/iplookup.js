6083d7d0cc6daa0ee0336fedb23e38e5fe0935cc
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-08-04 09:43:25 -0300

    asdwasd

--- commands/iplookup.js
+++ commands/iplookup.js
@@ -1,14 +1,18 @@
 const Discord = require("discord.js");
 const querystring = require('querystring');
+const fetch = require("node-fetch").default
 module.exports.run = async (bot, message, args,) => {
 
     const axios = require("axios")
-    axios.get(`https://proxycheck.io/v2/${message.content}?key=84x64h-y0084l-473709-154aq6&vpn=1&asn=1`)
-        .then((res) => {
-            console.log(res)
-                .catch(err => message.reply(`Something went wrong... ${err}`));
-        })
+    fetch(`https://proxycheck.io/v2/${message.content}?key=84x64h-y0084l-473709-154aq6&vpn=1&asn=1`)
+    .then(response => response.json()) //YOU ARE TURNING THE RESPONSE INTO A JSON FORMAT 
+    .then(data => {
+        console.log(response.data)
+
+    })
 }
+
+
 module.exports.config = {
     name: "lookup",
     description: "Sends a bot invite link nothing more.",
