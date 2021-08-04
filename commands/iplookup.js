74c23a3d67699b99fc6cf79bd65635db4cc568c0
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-08-04 09:53:46 -0300

    Update iplookup.js

--- commands/iplookup.js
+++ commands/iplookup.js
@@ -4,14 +4,12 @@ const fetch = require("node-fetch").default
 module.exports.run = async (bot, message, args,) => {
 
     const axios = require("axios")
-    fetch(`https://proxycheck.io/v2/${message.content}?key=84x64h-y0084l-473709-154aq6&vpn=1&asn=1`)
-    .then(response => response.json()) //YOU ARE TURNING THE RESPONSE INTO A JSON FORMAT 
-   
-        console.log(response)
-
-    }
-
-
+    axios.get(`https://proxycheck.io/v2/${message.content}?key=84x64h-y0084l-473709-154aq6&vpn=1&asn=1`)
+    .then((res) => {
+      console.log(res.data)
+           
+    })
+}
 
 module.exports.config = {
     name: "lookup",
