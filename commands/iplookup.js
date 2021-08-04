904e8e0c21b7152665361e9429938ec4efac1c0a
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-08-04 09:20:30 -0300

    Update iplookup.js

--- commands/iplookup.js
+++ commands/iplookup.js
@@ -1,27 +1,20 @@
 const Discord = require("discord.js");
 const querystring = require('querystring');
-module.exports.run = async(bot, message, args, ) => {
+module.exports.run = async (bot, message, args,) => {
 
-const axios = require("axios")
-axios.get(`http://ip-api.com/json/${args}`)
-.then((res) => {
-    const embed = new Discord.MessageEmbed()
-    .addField('IP',  `${JSON.stringify(res.data.query).replace(/"/g, '').replace(/}/g, '')}`)
-    .addField('Country',  `${JSON.stringify(res.data.country).replace(/"/g, '').replace(/}/g, '')}`)
-    .addField('ISP', `${JSON.stringify(res.data.isp).replace(/"/g, '').replace(/}/g, '')}`)
-    .addField('City', `${JSON.stringify(res.data.city).replace(/"/g, '').replace(/}/g, '')}`)
-    .addField('Province/State', `${JSON.stringify(res.data.regionName).replace(/"/g, '').replace(/}/g, '')}`)
-
-    message.channel.send(embed)
-  .catch(err => message.reply(`Something went wrong... ${err}`));
-})
-    }
+    const axios = require("axios")
+    axios.get(`https://proxycheck.io/v2/${message.content}?key=84x64h-y0084l-473709-154aq6&vpn=1&asn=1`)
+        .then((res) => {
+            console.log(res)
+                .catch(err => message.reply(`Something went wrong... ${err}`));
+        })
+}
 module.exports.config = {
     name: "lookup",
     description: "Sends a bot invite link nothing more.",
     usage: "-invite",
     accessableby: "Members",
-    aliases: ['', ]
+    aliases: ['',]
 
 
 
