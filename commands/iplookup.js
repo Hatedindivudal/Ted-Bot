b4ce3f35768085d332bbd218de80c76c6cdbc686
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-08-06 16:47:05 -0300

    Update iplookup.js

--- commands/iplookup.js
+++ commands/iplookup.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args,) => {
     const axios = require("axios")
     axios.get(`https://vpnapi.io/api/${args}?key=0d14f51ea0304c97a34e48a83f82a057`)
     .then((res) => {
-     console.log(res.data)
+     console.log(res.data.location)
     })
 }
 
