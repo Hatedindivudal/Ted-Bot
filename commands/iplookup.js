14904791ce4029323deb64c91a5ecd07188956cc
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-08-07 05:58:10 -0300

    Update iplookup.js

--- commands/iplookup.js
+++ commands/iplookup.js
@@ -9,10 +9,10 @@ module.exports.run = async (bot, message, args,) => {
         const embed = new Discord.MessageEmbed()
         .addFields(
             {name: 'IP', value: `${JSON.stringify(res.data.ip)}`, inline: true},
-            {name: 'Country', value: `${JSON.stringify(res.data.location.country).replace(/"/g, '').replace(/}/g, '')}`, inline: true},
-            {name: 'City', value: `${JSON.stringify(res.data.location.city).replace(/"/g, '').replace(/}/g, '')}`, inline: true},
-            {name: 'Continent', value: `${JSON.stringify(res.data.location.continent).replace(/"/g, '').replace(/}/g, '')}`, inline: true},
-            {name: 'ISP', value: `${JSON.stringify(res.data.network.autonomous_system_organization).replace(/"/g, '').replace(/}/g, '')}`, inline: true},
+            {name: 'Country', value: `${JSON.stringify(res.data.location.country)}`, inline: true},
+            {name: 'City', value: `${JSON.stringify(res.data.location.city)}`, inline: true},
+            {name: 'Continent', value: `${JSON.stringify(res.data.location.continent)}`, inline: true},
+            {name: 'ISP', value: `${JSON.stringify(res.data.network.autonomous_system_organization)}`, inline: true},
             
         )
         .setThumbnail('https://cdn.discordapp.com/attachments/869064794764873731/873488737609322496/3941-anime-aesthetic.gif')
