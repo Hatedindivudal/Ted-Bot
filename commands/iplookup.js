f52ca1fdacfdf35fa02219e8b07e1dc14f4a15de
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-08-07 05:53:35 -0300

    Update iplookup.js

--- commands/iplookup.js
+++ commands/iplookup.js
@@ -7,14 +7,16 @@ module.exports.run = async (bot, message, args,) => {
     axios.get(`https://vpnapi.io/api/${args}?key=0d14f51ea0304c97a34e48a83f82a057`)
     .then((res) => {
         const embed = new Discord.MessageEmbed()
+        .addTitle('')
         .addFields(
             {name: 'IP', value: `${JSON.stringify(res.data.ip)}`, inline: true},
-            {name: 'Country', value: `${JSON.stringify(res.data.location.country)}`, inline: true},
-            {name: 'City', value: `${JSON.stringify(res.data.location.city)}`, inline: true},
-            {name: 'Continent', value: `${JSON.stringify(res.data.location.continent)}`, inline: true},
-            {name: 'ISP', value: `${JSON.stringify(res.data.network.autonomous_system_organization)}`, inline: true},
+            {name: 'Country', value: `${JSON.stringify(res.data.location.country).replace(/"/g, '').replace(/}/g, '')}`, inline: true},
+            {name: 'City', value: `${JSON.stringify(res.data.location.city).replace(/"/g, '').replace(/}/g, '')}`, inline: true},
+            {name: 'Continent', value: `${JSON.stringify(res.data.location.continent).replace(/"/g, '').replace(/}/g, '')}`, inline: true},
+            {name: 'ISP', value: `${JSON.stringify(res.data.network.autonomous_system_organization).replace(/"/g, '').replace(/}/g, '')}`, inline: true},
             
         )
+        .setThumbnail('https://cdn.discordapp.com/attachments/869064794764873731/873488737609322496/3941-anime-aesthetic.gif')
     message.channel.send(embed)
     })
 }
