41ed381b42abae1ff91cca4027f928175490d921
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2021-08-08 12:38:51 -0300

    Update iplookup.js

--- commands/iplookup.js
+++ commands/iplookup.js
@@ -9,9 +9,9 @@ module.exports.run = async (bot, message, args,) => {
         const embed = new Discord.MessageEmbed()
         .addFields(
             {name: 'IP', value: `${JSON.stringify(res.data.ip).replace(/"/g, '')}`, inline: true},
-            {name: 'Country', value: `${JSON.stringify(res.data.location.country)}`, inline: true},
-            {name: 'City', value: `${JSON.stringify(res.data.location.city)}`, inline: true},
-            {name: 'Continent', value: `${JSON.stringify(res.data.location.continent)}`, inline: true},
+            {name: 'Country', value: `${JSON.stringify(res.data.location.country).replace(/"/g, '')}`, inline: true},
+            {name: 'City', value: `${JSON.stringify(res.data.location.city).replace(/"/g, '')}`, inline: true},
+            {name: 'Continent', value: `${JSON.stringify(res.data.location.continent).replace(/"/g, '')}`, inline: true},
             {name: 'ISP', value: `${JSON.stringify(res.data.network.autonomous_system_organization).replace(/"/g, '')}`, inline: true},
             
         )
