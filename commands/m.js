0be0d80443a93311c2b07d7c04713d37900114c5
Hatedindivudal <65380525+Hatedindivudal@users.noreply.github.com>
2022-07-01 04:11:12 -0300

    Update m.js

--- commands/m.js
+++ commands/m.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
 
 const { inspect } = require('util');
-if (message.author.id !== '961579072431988796')return;
+if (message.author.id !== '977041100051198003')return;
 
 
     
